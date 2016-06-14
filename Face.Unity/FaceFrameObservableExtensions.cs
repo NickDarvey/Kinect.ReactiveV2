098e87d2ba1ac63448df56f5714d993511c52206
Nick Darvey <nick@two-bulls.com>
2016-06-14 18:20:32 +1000

    Add face bits for Unity

---
+++ Face.Unity/FaceFrameObservableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Kinect.Face;
+using System;
+using UniRx;
+
+namespace Kinect.ReactiveV2
+{
+    public static class FaceFrameObservableExtensions
+    {
+        public static IObservable<FaceFrameResult> SelectFaceFrame(this IObservable<FaceFrameArrivedEventArgs> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            return source.Select(_ =>
+            {
+                using (var frame = _.FrameReference.AcquireFrame())
+                {
+                    if (frame == null) return null;
+
+                    return frame.FaceFrameResult;
+                }
+            })
+            .Where(_ => _ != null);
+        }
+    }
+}
---
+++ Face.Unity/FaceFrameReaderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Kinect.Face;
+using System;
+using UniRx;
+
+namespace Kinect.ReactiveV2
+{
+    public static class FaceFrameReaderExtensions
+    {
+        public static IObservable<FaceFrameArrivedEventArgs> FaceFrameArrivedObservable(this FaceFrameReader reader)
+        {
+            return Observable.FromEventPattern<EventHandler<FaceFrameArrivedEventArgs>, FaceFrameArrivedEventArgs>(
+                h => h.Invoke,
+                h => reader.FrameArrived += h,
+                h => reader.FrameArrived -= h)
+                .Select(_ => _.EventArgs);
+        }
+    }
+}
